f0553ba6813a680fd28395f62bb69798d541bdad
sergi <sergiich5@gmail.com>
2021-10-03 14:27:48 -0500

    updating 1st file

--- src/SetUp/First.java
+++ src/SetUp/First.java
@@ -2,7 +2,7 @@ package SetUp;
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello Chicago");
 
         // This is a new line
 
